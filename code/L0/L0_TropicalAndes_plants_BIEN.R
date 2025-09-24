11796be6031b9e566021893043a8c8a879d21125
Jenna Baljunas <jennabaljunas@gmail.com>
2025-09-24 09:33:14 -0400

    .R file copy, added "plant fruiting duration" as an additional trait

---
+++ code/L0/L0_TropicalAndes_plants_BIEN.R
@@ -0,0 +1,58 @@
+# title: "Tropical Andes plant trait data BIEN"
+# author: "Hazel J. Anderson"
+# project: "Plant-Frugivore Diversity"
+# collaborators: "Beth E. Gerstner, Phoebe L. Zarnetske, and Jenna B. Baljunas"
+# overview: "This script retrieves plant trait data from the BIEN database for Tropical Andean countries."
+# data input: "none"
+# data output: "TropicalAndes_BIEN_traits.csv, AllDesired_BIEN_plant_traits.csv"
+# date: "2025-09-22"
+
+
+# set file paths
+data_path_L0 <- file.path('G:/Shared drives/SpaCE_Lab_FRUGIVORIA/data/plants/L0')
+output_path_L0 <- file.path('G:/Shared drives/SpaCE_Lab_FRUGIVORIA/data/plants/L0')
+
+
+# load required packages
+library(BIEN); library(tidyr); library(dplyr)
+
+
+# retrieved desired trait data from BIEN-- data downloaded on 2025-09-22
+BIEN_trait_list()
+
+trait_list <- c("whole plant dispersal syndrome", "whole plant height", "maximum whole plant longevity", "maximum whole plant height", "minimum whole plant height", "longest whole plant longevity", "fruit type", "maximum fruit length", "minimum fruit length", "seed mass", "whole plant growth form", "whole plant growth form diversity", "plant fruiting duration")
+
+
+# list of desired countries
+TA_countries <- c("Bolivia", "Ecuador", "Venezuela", "Colombia", "Peru")
+
+
+# retrieve all BIEN records for desired traits
+AllDesired_BIEN_plant_traits <- BIEN_trait_trait(trait_list, all.taxonomy = TRUE, political.boundaries = TRUE, source.citation = TRUE)
+
+
+# retrieve all BIEN records for desired traits in Tropical Andes countries
+TropicalAndes_BIEN_plant_traits <- BIEN_trait_country(TA_countries, trait_list, all.taxonomy = TRUE, political.boundaries = TRUE, source.citation = TRUE)
+
+
+# data summary
+glimpse(AllDesired_BIEN_plant_traits)
+
+# Note to change this to the Git repo location,not computer
+source("C:/GitHub_projects/plant-frugivore diversity/neotropical_plants/code/data_summary.R")
+
+data_summary(AllDesired_BIEN_plant_traits, AllDesired_BIEN_plant_traits$scrubbed_species_binomial, AllDesired_BIEN_plant_traits$scrubbed_genus, AllDesired_BIEN_plant_traits$scrubbed_family)
+
+AllDesired_BIEN_plant_traits %>% count(trait_name)
+
+# desired traits in Tropical Andes countries
+glimpse(TropicalAndes_BIEN_plant_traits)
+
+data_summary(AllDesired_BIEN_plant_traits, TropicalAndes_BIEN_plant_traits$scrubbed_species_binomial, TropicalAndes_BIEN_plant_traits$scrubbed_genus, TropicalAndes_BIEN_plant_traits$scrubbed_family)
+
+TropicalAndes_BIEN_plant_traits %>% count(trait_name)
+
+
+# write data to csv
+write.csv(AllDesired_BIEN_plant_traits, file.path(output_path_L0,"AllDesired_BIEN_plant_traits.csv"))
+write.csv(TropicalAndes_BIEN_plant_traits, file.path(output_path_L0,"TropicalAndes_BIEN_plant_traits.csv"))
