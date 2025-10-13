8975b586e4d9a206607560702149fb880678e7b5
Jenna Baljunas <jennabaljunas@gmail.com>
2025-10-13 14:39:28 -0400

    Included data_summary function

--- code/L0/L0_TropicalAndes_plants_BIEN.R
+++ code/L0/L0_TropicalAndes_plants_BIEN.R
@@ -43,8 +43,13 @@ TropicalAndes_BIEN_plant_traits <- BIEN_trait_country(TA_countries, trait_list, 
 # data summary
 glimpse(AllDesired_BIEN_plant_traits)
 
-# Note to change this to the Git repo location,not computer
-source("C:/GitHub_projects/plant-frugivore diversity/neotropical_plants/code/data_summary.R")
+data_summary <- function(records, species, genera, families){
+  num_records <- nrow(records)
+  num_species <- length(unique(species))
+  num_genera <- length(unique(genera))
+  num_families <- length(unique(families))
+  return(cat("The number of records is", num_records, "\n", "The number of species is", num_species, "\n","The number of genera is", num_genera, "\n", "The number of families is", num_families))
+}
 
 data_summary(AllDesired_BIEN_plant_traits, AllDesired_BIEN_plant_traits$scrubbed_species_binomial, AllDesired_BIEN_plant_traits$scrubbed_genus, AllDesired_BIEN_plant_traits$scrubbed_family)
 
